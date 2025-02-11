891aa2625d3fb5f3164b2147af9d56857ee0e6de
Chen <jonkiky@gmail.com>
2025-02-11 13:29:24 -0500

    remove the github page settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    basePath: "/ctdc-data-model-navigator-landing", // GitHub repo name
-    assetPrefix: "/ctdc-data-model-navigator-landing/",
-    trailingSlash: true, // Ensures paths include a trailing slash
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
